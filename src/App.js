ddbacc21f9af5c431f240890743aafed978ac7d7
oieusouobixo <guicosta87@yahoo.com.br>
2020-08-09 22:37:10 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
+import Form from './Form';
 import Table from './Table';
 
 class App extends Component {
-    render() {
-        const characters = [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspiring Actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ];
+    state = {
+        characters: [],
+    
+    };
 
-    return (
+    removeCharacter = (index) => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            }),
+        });
+    };
+
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]});
+    };
+
+    render() {
+        const { characters } = this.state;
+        
+        return (
         <div className="container">
-            <Table characterData={characters} />
+            <Table characterData={characters} removeCharacter={this.removeCharacter} />
+            <Form handleSubmit={this.handleSubmit} />
         </div>
       )
     }
-  };
+};
 
   export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const TableHeader = () => { // simple component
+const TableHeader = () => {
     return (
         <thead>
             <tr>
@@ -11,21 +11,31 @@ const TableHeader = () => { // simple component
     )
 };
 
-const TableBody = () => { // simple component
-    return <tbody />;
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        )
+    })
+
+    return <tbody>{rows}</tbody>
 };
 
-class Table extends Component { // class component
-    render() {
-        const {characterData} = this.props;
+const Table = (props) => { 
+    const { characterData, removeCharacter } = props;
 
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={characterData} />
-            </table>
-        )
-    }
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
 };
 
 export default Table;
