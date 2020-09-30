55cc4a090fd67729036faffc6047c666e3955ad1
oieusouobixo <guicosta87@yahoo.com.br>
2020-09-30 15:34:49 -0300

    Updates

---
+++ precache-manifest.f1b2f555ff93f40f8bbbc1a32529aed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8684886f3e0fd556262c9c845881e29",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "00156cc204be124b8ec4",
+    "url": "/react-tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00156cc204be124b8ec4",
+    "url": "/react-tutorial/static/js/main.91287538.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
